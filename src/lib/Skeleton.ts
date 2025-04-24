844321eac7f19c2cc1541efded5a7f0cab713df6
Oscar Sims <oscar@oscarsims.co.nz>
2025-04-24 17:40:11 +0300

    Add toMultiPolygon()

--- src/lib/Skeleton.ts
+++ src/lib/Skeleton.ts
@@ -1,4 +1,4 @@
-import {MultiLineString} from "geojson";
+import {MultiLineString, MultiPolygon} from "geojson";
 import Vector2d from "./Primitives/Vector2d";
 import EdgeResult from "./EdgeResult";
 import {Dictionary, List} from "./Utils";
@@ -12,10 +12,33 @@ export class Skeleton {
 		this.distances = distances;
 	}
 
+	/**
+	 * Convert this skeleton to a MultiLineString.
+	 * @returns MultiLineString
+	 */
 	toMultiLineString(): MultiLineString {
 		const coordinates = this.edges.map(edgeRes =>
 			edgeRes.polygon.map(point => [point.x, point.y])
 		);
 		return { type: "MultiLineString", coordinates };
+	}
+
+	/**
+	 * Convert this skeleton to a MultiPolygon.
+	 * @returns MultiPolygon
+	 */
+	toMultiPolygon(): MultiPolygon {
+		const coordinates = this.edges.map(edgeRes => {
+			const ring = edgeRes.polygon.map(point => [point.x, point.y]);
+			if (ring.length > 0) {
+				const first = ring[0];
+				const last = ring[ring.length - 1];
+				if (first[0] !== last[0] || first[1] !== last[1]) {
+					ring.push(first);
+				}
+			}
+			return [ring];
+		});
+		return { type: "MultiPolygon", coordinates };
 	}
 }
