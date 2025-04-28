290d021d028503c231f0fbf38c7c5ef5adfe9563
Oscar Sims <oscar@oscarsims.co.nz>
2025-04-28 20:58:13 +0300

    Add more test cases

--- src/test/test.ts
+++ src/test/test.ts
@@ -22,8 +22,8 @@ const directories = {
 //     };
 // });
 
-test("straight-skeleton-geojson - Floating point epsilon handling", (t) => {
-    const poly: MultiPolygon = {
+test("straight-skeleton-geojson - Floating point epsilon handling at different scales", (t) => {
+    const poly1: MultiPolygon = {
         type: "MultiPolygon",
         coordinates: [
             [
@@ -38,6 +38,41 @@ test("straight-skeleton-geojson - Floating point epsilon handling", (t) => {
         ]
     };
 
-    t.doesNotThrow(() => SkeletonBuilder.buildFromGeoJSON(poly));
+    t.doesNotThrow(() => SkeletonBuilder.buildFromGeoJSON(poly1));
+
+    const poly2: MultiPolygon = {
+        type: "MultiPolygon",
+        coordinates: [
+            [
+                [
+                    [-0.2200085430165132, 0.806050096020618],
+                    [-0.4322383282246447, 0.6936438066656281],
+                    [-0.43967527961409397, 0.4816906922093666],
+                    [-0.17790995318322652, 0.5167726431738992],
+                    [-0.2200085430165132, 0.806050096020618]
+                ]
+            ]
+        ]
+    };
+
+    t.doesNotThrow(() => SkeletonBuilder.buildFromGeoJSON(poly2));
+
+    const poly3: MultiPolygon = {
+        type: "MultiPolygon",
+        coordinates: [
+            [
+                [
+                    [-22.00085430165132, 80.6050096020618],
+                    [-43.22383282246447, 69.36438066656281],
+                    [-43.967527961409397, 48.16906922093666],
+                    [-17.790995318322652, 51.67726431738992],
+                    [-22.00085430165132, 80.6050096020618]
+                ]
+            ]
+        ]
+    };
+
+    t.doesNotThrow(() => SkeletonBuilder.buildFromGeoJSON(poly3));
+
     t.end();
 });
